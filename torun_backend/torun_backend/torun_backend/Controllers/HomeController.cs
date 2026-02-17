8530c9fb837c38ef49464e97ff7a788d6bd1b168
Umut Güven <usluumutguven@gmail.com>
2026-02-17 19:07:00 +0300

    api installed

---
+++ torun_backend/torun_backend/torun_backend/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace torun_backend.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            string deneme = "deneme";
+            return deneme;
+        }
+    }
+}
